7c1f72fe319c0eeeaccfa849f02c34631b4c5986
shir <shir.reifenberg@gmail.com>
2022-08-26 14:28:52 +0300

    fix

--- utils/split_to_train_and_test.py
+++ utils/split_to_train_and_test.py
@@ -8,17 +8,19 @@ from numpy import identity
 # This function performs some operations required before splitting the data to test and train:
 # 1. removes all .xml files
 # 2. creates a unique name for each folder and photo
+# 3. remove the redundent /images nested folder
 def chunk_preproccess(root_folder):
     chunk_name = root_folder.split('\\')
-    print (f'chunk name is: {chunk_name}')
+    print (f'chunk name is: {chunk_name[-1]}')
     for f in os.listdir(root_folder):
         identity_dir = os.path.join(root_folder, f)
         if os.path.isdir(identity_dir):
             # change the directory name
             new_dir = os.path.join(root_folder, chunk_name[-1]+'_'+f)
             os.rename(identity_dir, new_dir)
-            for file_name in os.listdir(new_dir):
-                file_path = os.path.join(new_dir, file_name)
+            images_dir = os.path.join(new_dir+'\\images')
+            for file_name in os.listdir(images_dir):
+                file_path = os.path.join(images_dir, file_name)
                 # remove file if its xml
                 if (file_name.lower().endswith(('.xml'))):
                     file_to_delete = file_path
